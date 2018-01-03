8718b37642e2e27a015a0d7c878c57df28825de0
Unknown <Frederik_doktor@phys.au.dk>
2018-01-03 19:01:19 +0100

    GUI 1.1
    Added extra permanent figures which can be put on display using the buttons

--- GUI/ownscript/testing.py
+++ GUI/ownscript/testing.py
@@ -28,7 +28,7 @@ class PrettyWidget(QtGui.QWidget):
         
         
     def initUI(self):
-        self.setGeometry(600,300, 1000, 600)
+        self.setGeometry(600,300, 1500, 600)
         self.center()
         self.setWindowTitle('HK data')     
        
@@ -38,34 +38,57 @@ class PrettyWidget(QtGui.QWidget):
         btn1 = QtGui.QPushButton('Plot 1', self)
         btn1.resize(btn1.sizeHint()) 
         btn1.clicked.connect(self.plot1)
-        grid.addWidget(btn1, 3,0)
+        grid.addWidget(btn1, 3,0,1,2)
 
         btn2 = QtGui.QPushButton('Plot 2', self)
         btn2.resize(btn2.sizeHint())    
         btn2.clicked.connect(self.plot2)
-        grid.addWidget(btn2, 3,1)
-        
-        "more buttons for more plots"
-        
+        grid.addWidget(btn2, 3,2,1,2)
         
         btn3 = QtGui.QPushButton('Plot 3', self)
         btn3.resize(btn3.sizeHint())    
         btn3.clicked.connect(self.plot3)
-        grid.addWidget(btn3, 4,0)
+        grid.addWidget(btn3, 4,0,1,2)
         
         btn4 = QtGui.QPushButton('Plot 4', self)
         btn4.resize(btn4.sizeHint())    
         btn4.clicked.connect(self.plot4)
-        grid.addWidget(btn4, 4,1)
+        grid.addWidget(btn4, 4,2,1,2)
         
+        
+        self.figure1 = plt.figure(figsize=(1,2))
+        self.canvas1 = FigureCanvas(self.figure1)
+        grid.addWidget(self.canvas1, 2,0)
+
+        self.figure2 = plt.figure(figsize=(2,2))
+        self.canvas2 = FigureCanvas(self.figure2)
+        grid.addWidget(self.canvas2, 2,1)
+        
+        self.figure3 = plt.figure(figsize=(2,2))
+        self.canvas3 = FigureCanvas(self.figure3)
+        grid.addWidget(self.canvas3, 2,2)
+        
+        self.figure4 = plt.figure(figsize=(1,2))
+        self.canvas4 = FigureCanvas(self.figure4)
+        grid.addWidget(self.canvas4, 2,3)
+        hk4=self.figure4.add_subplot(111)
+        x4 = [i for i in range(100)]
+        y4 = [i**10 for i in x4]
+        hk4.plot(x4,y4)
+        self.canvas4.draw()
+        
+        
+        #a2 = QtGui.QPushButton('fig2', self)
+        #a2.resize(a2.sizeHint())    
+       # a2.clicked.connect(self.plot2)
+        #grid.addWidget(a2, 2,1)        
         "figure and toolbar"
                
-        self.figure = plt.figure(figsize=(15,5))    
+        self.figure = plt.figure(figsize=(30,10))    
         self.canvas = FigureCanvas(self.figure)     
         self.toolbar = NavigationToolbar(self.canvas, self)
-        grid.addWidget(self.canvas, 1,0,1,2)
-        grid.addWidget(self.toolbar, 0,0,1,2)
-               
+        grid.addWidget(self.canvas, 1,0,1,4)
+        grid.addWidget(self.toolbar, 0,0,1,4)
         self.show()
     
     "Will try to plot HK data over each button"
@@ -79,6 +102,8 @@ class PrettyWidget(QtGui.QWidget):
         y = [i**2 for i in x]
         ax.plot(x,y, 'b.-')
         ax.set_title('Quadratic Plot')
+        ax.set_xlabel('time')
+        ax.set_ylabel('maybe temp')
         self.canvas.draw()
     
     
